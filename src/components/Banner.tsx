ea4f531fdda605fc1a63cde75945642651fcc4f4
VictorMello1993 <victorsmello93@gmail.com>
2022-10-29 20:53:46 -0300

    card course (part 3)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,25 +3,23 @@ import styled from "styled-components";
 
 export function Banner() {
   return (
-    <>
-      <BannerElement>
-        <div className="image-container">
-          <div className="image">
-            <img
-              src="https://img-c.udemycdn.com/notices/featured_banner/image_udlite/1cd05c12-f169-45ae-a452-adb8c81739b4.jpg"
-              alt="Banner"
-            />
-          </div>
-          <div className="banner-description">
-            <h1>Encontre o melhor para você</h1>
-            <p>
-              Muitos cursos oferecem prévias gratuitas para que você possa conferir o conteúdo e o estilo de ensino. Dê
-              uma olhada.
-            </p>
-          </div>
+    <BannerElement>
+      <div className="image-container">
+        <div className="image">
+          <img
+            src="https://img-c.udemycdn.com/notices/featured_banner/image_udlite/1cd05c12-f169-45ae-a452-adb8c81739b4.jpg"
+            alt="Banner"
+          />
         </div>
-      </BannerElement>
-    </>
+        <div className="banner-description">
+          <h1>Encontre o melhor para você</h1>
+          <p>
+            Muitos cursos oferecem prévias gratuitas para que você possa conferir o conteúdo e o estilo de ensino. Dê
+            uma olhada.
+          </p>
+        </div>
+      </div>
+    </BannerElement>
   );
 }
 
--- src/layout/MainContent.tsx
+++ src/layout/MainContent.tsx
@@ -10,37 +10,39 @@ export function MainContent() {
         <Banner />
         <CoursesContainer>
           <CourseItem>
-            <div className="title-container">
-              <div className="title">
-                <h2>Recomendados para você</h2>
+            <div className="course-item-wrapper">
+              <div className="title-container">
+                <div className="title">
+                  <h2>Recomendados para você</h2>
+                </div>
               </div>
-            </div>
-            <div className="card-container">
-              <Card
-                data-index="0"
-                description="Curso Java"
-                src="https://img-c.udemycdn.com/course/240x135/1701388_0134.jpg"
-              />
-              <Card
-                data-index="1"
-                description="Curso Javascript e Typescript Avançado"
-                src="https://img-c.udemycdn.com/course/240x135/2575266_c184_4.jpg"
-              />
-              <Card
-                data-index="2"
-                description="Curso Java 2022"
-                src="https://img-c.udemycdn.com/course/240x135/870252_cf52_8.jpg"
-              />
-              <Card
-                description="Desenvolvimento Web Completo 2022"
-                data-index="3"
-                src="https://img-c.udemycdn.com/course/240x135/1341268_c20e_3.jpg"
-              />
-              <Card
-                data-index="4"
-                description="Curso Docker"
-                src="https://img-c.udemycdn.com/course/240x135/1137616_870b.jpg"
-              />
+              <div className="card-container">
+                <Card
+                  data-index="0"
+                  description="Curso Java"
+                  src="https://img-c.udemycdn.com/course/240x135/1701388_0134.jpg"
+                />
+                <Card
+                  data-index="1"
+                  description="Curso Javascript e Typescript Avançado"
+                  src="https://img-c.udemycdn.com/course/240x135/2575266_c184_4.jpg"
+                />
+                <Card
+                  data-index="2"
+                  description="Curso Java 2022"
+                  src="https://img-c.udemycdn.com/course/240x135/870252_cf52_8.jpg"
+                />
+                <Card
+                  description="Desenvolvimento Web Completo 2022"
+                  data-index="3"
+                  src="https://img-c.udemycdn.com/course/240x135/1341268_c20e_3.jpg"
+                />
+                <Card
+                  data-index="4"
+                  description="Curso Docker"
+                  src="https://img-c.udemycdn.com/course/240x135/1137616_870b.jpg"
+                />
+              </div>
             </div>
           </CourseItem>
         </CoursesContainer>
@@ -55,12 +57,16 @@ const MainContentElement = styled.main`
 
 const CoursesContainer = styled.div`
   margin: 48px auto;
-  width: 100%;
   height: 337px;
 `;
 
 const CourseItem = styled.div`
   height: 290px;
+
+  .course-item-wrapper {
+    margin: 0 auto;
+    width: 85%;
+  }
 
   .title {
     display: flex;
