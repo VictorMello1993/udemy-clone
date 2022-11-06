9d14eb21403072234268128998e118cbdd3af41d
VictorMello1993 <victorsmello93@gmail.com>
2022-11-06 15:29:36 -0300

    Course detail desktop version

--- src/layout/CourseItemDetailContent.tsx
+++ src/layout/CourseItemDetailContent.tsx
@@ -1,35 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { MainContentElement } from "./MainContent";
 
 export function CourseItemDetailContent() {
   return (
-    <CourseItemDetailTopContainer>
-      <div className="course-detail">
-        <div className="course-detail image">
-          <img src="https://img-c.udemycdn.com/course/480x270/1137616_870b.jpg" alt="Curso Docker" />
+    <MainContentElement>
+      <CourseItemDetailTopContainer>
+        <div className="course-detail">
+          <div className="course-detail image">
+            <img src="https://img-c.udemycdn.com/course/480x270/1137616_870b.jpg" alt="Curso Docker" />
+          </div>
+          <div className="course-detail text">
+            <div className="course-detail title">
+              <h2>Docker: Ferramenta essencial para desenvolvedores</h2>
+            </div>
+            <div className="course-detail description">Aprenda a criar ambientes profissionais completos com Docker, que é a principal tecnologia de containers do mercado</div>
+            <div className="course-detail info">
+              <div className="rating">4.7</div>
+              <div className="rating-count">9185</div>
+              <div className="students-subscribed">22934</div>
+            </div>
+            <div className="course-detail created-by">Criado por Leonardo Leitão</div>
+            <div className="course-detail last-info">
+              <div className="course-detail last-update">Última atualização: 08/2022</div>
+              <div className="course-detail language">Português</div>
+            </div>
+            <div className="course-detail price">R$ 30.00</div>
+          </div>
         </div>
-        <div className="course-detail text">
-          <div className="course-detail title">
-            <h2>Docker: Ferramenta essencial para desenvolvedores</h2>
-          </div>
-          <div className="course-detail description">Aprenda a criar ambientes profissionais completos com Docker, que é a principal tecnologia de containers do mercado</div>
-          <div className="course-detail info">
-            <div className="rating">4.7</div>
-            <div className="rating-count">9185</div>
-            <div className="students-subscribed">22934</div>
-          </div>
-          <div className="course-detail created-by">Criado por Leonardo Leitão</div>
-          <div className="course-detail last-info">
-            <div className="course-detail last-update">Última atualização: 08/2022</div>
-            <div className="course-detail language">Português</div>
-          </div>
-          <div className="course-detail price">R$ 30.00</div>
-          <div className="course-detail trial-free-button">
-            <a href="/">Experimente o Plano individual gratuitamente</a>
-          </div>
+        <div className="try-free-button">
+          <a href="/">Experimente o Plano individual gratuitamente</a>
         </div>
-      </div>
-    </CourseItemDetailTopContainer>
+      </CourseItemDetailTopContainer>
+    </MainContentElement>
   );
 }
 
@@ -37,13 +40,16 @@ const CourseItemDetailTopContainer = styled.div`
   background-color: #1c1d1f;
   height: 100vh;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   .course-detail img {
     max-width: 100%;
   }
 
   .course-detail:not(:first-child) {
-    margin-top: 10px;
+    margin-top: 15px;
     font-family: udemy sans, -apple-system, BlinkMacSystemFont, Roboto, segoe ui, Helvetica, Arial, sans-serif, apple color emoji, segoe ui emoji, segoe ui symbol;
   }
 
@@ -64,14 +70,15 @@ const CourseItemDetailTopContainer = styled.div`
     font-weight: 500;
   }
 
-  .course-detail .trial-free-button {
+  .try-free-button {
     background-color: #a435f0;
     padding: 12px 12px;
     text-align: center;
     cursor: pointer;
+    border-radius: 8px;
   }
 
-  .course-detail .trial-free-button:hover {
+  .try-free-button:hover {
     background-color: #8315cd;
   }
 
@@ -79,7 +86,7 @@ const CourseItemDetailTopContainer = styled.div`
     font-size: 30px;
   }
 
-  .course-detail .trial-free-button a {
+  .try-free-button a {
     color: #fff;
     text-decoration: none;
     font-weight: 700;
@@ -87,13 +94,28 @@ const CourseItemDetailTopContainer = styled.div`
 
   @media (min-width: 600px) {
     .course-detail {
-      float: right;
+      width: 75%;
     }
+    /* .course-detail {
+      display: flex;
+      flex-direction: column;
+      margin-top: 10vh;
+      width: 50%;
+    } */
+
+    /* .course-detail .text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    } */
   }
 
   @media (max-width: 600px) {
     .course-detail .trial-free-button {
       margin-top: 15vh;
+    }
+    .course-detail {
+      width: 100%;
     }
   }
 `;
